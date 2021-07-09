ef98cc505d2108eff69fe1f3ff9dfdc4ed60f15d
Diwakar <diwakarn1599@gmail.com>
2021-07-10 00:42:49 +0530

    [Diwakar] usecase 1 Linkedlist created with 3 elements

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        Node head;
+        public void InsertAtLast(int val)
+        {
+            Node temp = new Node(val);
+            if (head == null)
+            { 
+                this.head = temp;
+            }
+            else
+            {
+                Node traverse = this.head;
+                while(traverse.next != null)
+                {
+                    traverse = traverse.next;
+                }
+                traverse.next = temp;
+             }
+
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            while(temp != null)
+            {
+                if(temp.next != null)
+                {
+                    Console.Write($"{temp.data}-->");
+                }
+                else
+                {
+                    Console.Write(temp.data);
+                }
+                
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,6 +7,11 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to linked list");
+            LinkedList list = new LinkedList();
+            list.InsertAtLast(56);
+            list.InsertAtLast(30);
+            list.InsertAtLast(70);
+            list.Display();
         }
     }
 }
